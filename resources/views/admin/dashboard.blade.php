d51d79a70b34dadd9fb80fbffc7b32a391529aac
coreoceantesting <coreoceantesting@gmail.com>
2025-01-28 18:23:57 +0530

    dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,61 +6,16 @@
     <div class="container-fluid">
     <!-- resources/views/dashboard.blade.php -->
     <div class="row">
-    <!-- Approved Couple Card -->
-        <div class="col-md-4 col-sm-12">
-            <div class="card card-animate">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                        <div>
-                            <p class="fw-medium text-muted mb-0">Approved Couple</p>
-                            <h2 class="mt-4 ff-secondary fw-semibold">
-                                <span class="counter-value" data-target="{{ $approvedCouple }}">0</span>
-                            </h2>
-                        </div>
-                        <div>
-                            <div class="avatar-sm flex-shrink-0">
-                                <span class="avatar-title bg-info-subtle rounded-circle fs-2">
-                                    <i data-feather="users" class="text-info"></i>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    <!-- Rejected Couple Card -->
-        <div class="col-md-4 col-sm-12">
-            <div class="card card-animate">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                        <div>
-                            <p class="fw-medium text-muted mb-0">Rejected Couple</p>
-                            <h2 class="mt-4 ff-secondary fw-semibold">
-                                <span class="counter-value" data-target="{{ $rejectedCouple }}">0</span>
-                            </h2>
-                        </div>
-                        <div>
-                            <div class="avatar-sm flex-shrink-0">
-                                <span class="avatar-title bg-info-subtle rounded-circle fs-2">
-                                    <i data-feather="users" class="text-info"></i>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    <!-- Approved Group Card -->
+   
+      <!-- Total People Visited Card -->
         <div class="col-md-4">
             <div class="card card-animate">
                 <div class="card-body">
                     <div class="d-flex justify-content-between">
                         <div>
-                            <p class="fw-medium text-muted mb-0">Approved Group</p>
+                            <p class="fw-medium text-muted mb-0">Total People Visited</p>
                             <h2 class="mt-4 ff-secondary fw-semibold">
-                                <span class="counter-value" data-target="{{ $approvedGroup }}">0</span>
+                                <span class="counter-value" data-target="{{ $totalPeopleVisited }}">0</span>
                             </h2>
                         </div>
                         <div>
@@ -74,6 +29,54 @@
                 </div>
             </div>
         </div>
+    
+       <!-- Female Entry Card -->
+        <div class="col-md-4">
+            <div class="card card-animate">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <div>
+                            <p class="fw-medium text-muted mb-0">Female Entry</p>
+                            <h2 class="mt-4 ff-secondary fw-semibold">
+                                <span class="counter-value" data-target="{{ $femaleEntry }}">0</span>
+                            </h2>
+                        </div>
+                        <div>
+                            <div class="avatar-sm flex-shrink-0">
+                                <span class="avatar-title bg-info-subtle rounded-circle fs-2">
+                                    <i data-feather="users" class="text-info"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    
+       <!-- Male Entry Card -->
+        <div class="col-md-4">
+            <div class="card card-animate">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <div>
+                            <p class="fw-medium text-muted mb-0">Male Entry</p>
+                            <h2 class="mt-4 ff-secondary fw-semibold">
+                                <span class="counter-value" data-target="{{ $maleEntry }}">0</span>
+                            </h2>
+                        </div>
+                        <div>
+                            <div class="avatar-sm flex-shrink-0">
+                                <span class="avatar-title bg-info-subtle rounded-circle fs-2">
+                                    <i data-feather="users" class="text-info"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 
     <!-- Rejected Group Card -->
         <div class="col-md-6">
@@ -98,15 +101,15 @@
             </div>
         </div>
 
-    <!-- Total People Visited Card -->
-        <div class="col-md-6">
+    <!-- Approved Couple Card -->
+    <div class="col-md-6 col-sm-12">
             <div class="card card-animate">
                 <div class="card-body">
                     <div class="d-flex justify-content-between">
                         <div>
-                            <p class="fw-medium text-muted mb-0">Total People Visited</p>
+                            <p class="fw-medium text-muted mb-0">Approved Couple</p>
                             <h2 class="mt-4 ff-secondary fw-semibold">
-                                <span class="counter-value" data-target="{{ $totalPeopleVisited }}">0</span>
+                                <span class="counter-value" data-target="{{ $approvedCouple }}">0</span>
                             </h2>
                         </div>
                         <div>
@@ -121,15 +124,15 @@
             </div>
         </div>
 
-    <!-- Male Entry Card -->
-        <div class="col-md-6">
+    <!-- Approved Group Card -->
+    <div class="col-md-6">
             <div class="card card-animate">
                 <div class="card-body">
                     <div class="d-flex justify-content-between">
                         <div>
-                            <p class="fw-medium text-muted mb-0">Male Entry</p>
+                            <p class="fw-medium text-muted mb-0">Approved Group</p>
                             <h2 class="mt-4 ff-secondary fw-semibold">
-                                <span class="counter-value" data-target="{{ $maleEntry }}">0</span>
+                                <span class="counter-value" data-target="{{ $approvedGroup }}">0</span>
                             </h2>
                         </div>
                         <div>
@@ -144,15 +147,15 @@
             </div>
         </div>
 
-    <!-- Female Entry Card -->
-        <div class="col-md-6">
+    <!-- Rejected Couple Card -->
+    <div class="col-md-6 col-sm-12">
             <div class="card card-animate">
                 <div class="card-body">
                     <div class="d-flex justify-content-between">
                         <div>
-                            <p class="fw-medium text-muted mb-0">Female Entry</p>
+                            <p class="fw-medium text-muted mb-0">Rejected Couple</p>
                             <h2 class="mt-4 ff-secondary fw-semibold">
-                                <span class="counter-value" data-target="{{ $femaleEntry }}">0</span>
+                                <span class="counter-value" data-target="{{ $rejectedCouple }}">0</span>
                             </h2>
                         </div>
                         <div>
